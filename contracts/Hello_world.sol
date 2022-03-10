84aae7a84ac93d948fae681643a2d833ed56f3e3
Remix Workspace <>
2022-03-10 08:51:38 +0100

    initial commit for hello world smart contract

---
+++ contracts/Hello_world.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Hello {
+    string public name;
+    string public helloPrefix = "Hello ";
+
+    constructor(string memory initialName){
+        name = initialName;
+    }
+
+    function setName(string memory newName) public {
+        name = newName;
+    }
+
+    function getHello()public view returns (string memory){
+        return string(abi.encodePacked(helloPrefix, name));
+    }
+}
